3c2bb476ebf65ec9d5d706f95e4d9d195b86ce95
Daringcuteseal <daringcuteseal@gmail.com>
2025-01-12 10:00:08 +0700

    plug: add RaafatTurki/hex.nvim

--- lua/ide/plugs.lua
+++ lua/ide/plugs.lua
@@ -37,6 +37,7 @@ require("lazy").setup(
 		{ "nvim-lualine/lualine.nvim", dependencies = {'nvim-tree/nvim-web-devicons'} },
 		"glepnir/dashboard-nvim",
 		"VonHeikemen/lsp-zero.nvim",
+		"RaafatTurki/hex.nvim",
 		"folke/noice.nvim",
 		"neovim/nvim-lspconfig",
 		"williamboman/mason.nvim",
@@ -909,3 +910,25 @@ require("nvim-tree").setup({
 		dotfiles = true,
 	},
 })
+
+-- defaults
+require 'hex'.setup {
+
+  -- cli command used to dump hex data
+  dump_cmd = 'xxd -g 1 -u',
+
+  -- cli command used to assemble from hex data
+  assemble_cmd = 'xxd -r',
+  
+  -- function that runs on BufReadPre to determine if it's binary or not
+  is_file_binary_pre_read = function()
+    -- logic that determines if a buffer contains binary data or not
+    -- must return a bool
+  end,
+
+  -- function that runs on BufReadPost to determine if it's binary or not
+  is_file_binary_post_read = function()
+    -- logic that determines if a buffer contains binary data or not
+    -- must return a bool
+  end,
+}
