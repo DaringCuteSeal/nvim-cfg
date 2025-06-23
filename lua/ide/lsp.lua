400b263281344205383e7a32b1fb20852147870b
DaringCuteSeal <daringcuteseal@gmail.com>
2025-06-23 11:22:00 +0700

    update configs and change keymap

--- lua/ide/lsp.lua
+++ lua/ide/lsp.lua
@@ -3,12 +3,12 @@ local lsp = require('lsp-zero')
 
 local cmp = require('cmp')
 local cmp_select = {behaviour = cmp.SelectHevari}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	['<C-Space>'] = cmp.mapping.complete(),
-})
+-- local cmp_mappings = lsp.defaults.cmp_mappings({
+-- 	['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+-- 	['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+-- 	['<C-y>'] = cmp.mapping.confirm({ select = true }),
+-- 	['<C-Space>'] = cmp.mapping.complete(),
+-- })
 
 local luasnip = require('luasnip')
 cmp.setup({
