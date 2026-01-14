782008ab9eccad886ea4c67beabd88ab423d6669
DaringCuteSeal <daringcuteseal@gmail.com>
2026-01-14 19:09:53 +0700

    add vimtex

--- lua/ide/plugs.lua
+++ lua/ide/plugs.lua
@@ -124,6 +124,16 @@ require("lazy").setup(
 		}
 		},
 		{
+			"lervag/vimtex",
+			lazy = false,     -- we don't want to lazy load VimTeX
+			-- tag = "v2.15", -- uncomment to pin to a specific release
+			init = function()
+				-- VimTeX configuration goes here, e.g.
+				vim.g.vimtex_view_general_viewer = "evince"
+				vim.g.vimtex_view_general_options = "@pdf"
+			end
+		},
+		{
 			'nvimdev/dashboard-nvim',
 			event = 'VimEnter',
 			config = function()
