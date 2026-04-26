a733592931d44ae5d55aee945553bb1e53115d51
DaringCuteSeal <daringcuteseal@gmail.com>
2026-04-26 10:51:10 +0700

    replace deprecated diagnostic goto_next and goto_prev

--- lua/ide/lsp.lua
+++ lua/ide/lsp.lua
@@ -39,80 +39,80 @@ cmp.setup({
 	},
 })
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('user_lsp_attach', {clear = true}),
-  callback = function(event)
-    local opts = {buffer = event.buf}
+	group = vim.api.nvim_create_augroup('user_lsp_attach', { clear = true }),
+	callback = function(event)
+		local opts = { buffer = event.buf }
 
-    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set('n', '<leader>vs', function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set('n', '<leader>d', function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set('n', ']d', function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set('n', '[d', function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set('n', '<leader>va', function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set('n', '<leader>vr', function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set('n', '<F2>', function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
-  end,
+		vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+		vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+		vim.keymap.set('n', '<leader>vs', function() vim.lsp.buf.workspace_symbol() end, opts)
+		vim.keymap.set('n', '<leader>d', function() vim.diagnostic.open_float() end, opts)
+		vim.keymap.set('n', ']d', function() vim.diagnostic.jump({ count = 1, float = true }) end, opts)
+		vim.keymap.set('n', '[d', function() vim.diagnostic.jump({ count = -1, float = true }) end, opts)
+		vim.keymap.set('n', '<leader>va', function() vim.lsp.buf.code_action() end, opts)
+		vim.keymap.set('n', '<leader>vr', function() vim.lsp.buf.rename() end, opts)
+		vim.keymap.set('n', '<F2>', function() vim.lsp.buf.rename() end, opts)
+		vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+	end,
 })
 
 local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'clangd', 'rust_analyzer'},
-  handlers = {
-    function(server_name)
-      vim.lsp.config[server_name].setup({
-        capabilities = lsp_capabilities,
-      })
-    end,
-  }
+	ensure_installed = { 'clangd', 'rust_analyzer' },
+	handlers = {
+		function(server_name)
+			vim.lsp.config[server_name].setup({
+				capabilities = lsp_capabilities,
+			})
+		end,
+	}
 })
 
 vim.diagnostic.config({
-    virtual_text = true
+	virtual_text = true
 })
 
 require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'moonfly',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
+	options = {
+		icons_enabled = true,
+		theme = 'moonfly',
+		component_separators = { left = '', right = '' },
+		section_separators = { left = '', right = '' },
+		disabled_filetypes = {
+			statusline = {},
+			winbar = {},
+		},
+		ignore_focus = {},
+		always_divide_middle = true,
+		globalstatus = false,
+		refresh = {
+			statusline = 1000,
+			tabline = 1000,
+			winbar = 1000,
+		}
+	},
+	sections = {
+		lualine_a = { 'mode' },
+		lualine_b = { 'branch', 'diff', 'diagnostics' },
+		lualine_c = { 'filename' },
+		lualine_x = { 'encoding', 'fileformat', 'filetype' },
+		lualine_y = { 'progress' },
+		lualine_z = { 'location' }
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { 'filename' },
+		lualine_x = { 'location' },
+		lualine_y = {},
+		lualine_z = {}
+	},
+	tabline = {},
+	winbar = {},
+	inactive_winbar = {},
+	extensions = {}
 }
 
 
@@ -124,5 +124,5 @@ vim.cmd([[let g:ale_rust_cargo_clippy_options = '-- -W clippy::nursery -W clippy
 
 -- inlay hints uwu
 if vim.lsp.inlay_hint then
-  vim.lsp.inlay_hint.enable(true, { 0 })
+	vim.lsp.inlay_hint.enable(true, { 0 })
 end
